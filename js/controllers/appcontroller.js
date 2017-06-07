1e359dbf79ca751e2e229340a115b2ec1c9c2b9c
Khuram <knshaikh@gmail.com>
2017-06-07 10:41:24 -0600

    added templatePicker and default editor widget

--- js/controllers/appcontroller.js
+++ js/controllers/appcontroller.js
@@ -1,10 +1,22 @@
 /*global define */
 define([
+  'dojo/_base/array',
   'controllers/mapcontroller',
+  'esri/dijit/editing/TemplatePicker',
+  'esri/IdentityManager',
   'widgets/edit/editTools'
-], function (MapController, EditTools) {
+], function (array, MapController, TemplatePicker, EditTools) {
 
   function mapLoaded(map) {
+    var requestLayer, layers = [], templatePicker;
+    requestLayer = map.getLayer('Requests');
+    layers.push(requestLayer);
+    templatePicker = new TemplatePicker({
+      featureLayers: layers,
+      rows: 'auto',
+      columns: 1
+    }, 'template-div');
+    templatePicker.startup();
     // var editTools = new EditTools({
     //   map: map
     // }, 'map-tools');
